c4739065d4f0caef8295a6b9795b19af542a3b16
Aemony <aemony@aemony.se>
2025-07-11 13:34:09 +0200

    Removed unused code

--- MediaWiki.psm1
+++ MediaWiki.psm1
@@ -487,61 +487,6 @@ function Copy-Object
 #endregion
 
 #region ConvertFrom-JsonToHashtable
-
-<#
-  .SYNOPSIS
-    Helper function to take a JSON string and turn it into a hashtable
-  .DESCRIPTION
-    The built in ConvertFrom-Json file produces as PSCustomObject that has case-insensitive keys. This means that
-    if the JSON string has different keys but of the same name, e.g. 'size' and 'Size' the comversion will fail.
-    Additionally to turn a PSCustomObject into a hashtable requires another function to perform the operation.
-    This function does all the work in step using the JavaScriptSerializer .NET class
-  .LINK
-    https://www.powershellgallery.com/packages/Poshstache/0.1.0/Content/ConvertFrom-JsonToHashtable.ps1
-  .NOTES
-    Copyright 2014 ASOS.com Limited
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-      http://www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-#>
-<#
-function ConvertFrom-JsonToHashtable
-{
-  [CmdletBinding()]
-  param (
-
-    [Parameter(Mandatory, Position = 0, ValueFromPipeline = $true)]
-    [AllowNull()]
-    [string]$InputObject,
-    [switch]$CaseSensitive # Switch to denote that the returning object should be case sensitive
-  )
-
-  # Perform a test to determine if the InputObject is null, if it is then return an empty hash table
-  if ([string]::IsNullOrEmpty($InputObject))
-  { $Dict = @{ } }
-  else
-  {
-    # load the required dll
-    [void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
-    $Deserializer = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
-    $Deserializer.MaxJsonLength = [int32]::MaxValue
-    $Dict = $Deserializer.DeserializeObject($InputObject)
-
-    # if the CaseSensitive is false then make the dictionary case insensitive
-    if ($CaseSensitive -eq $false)
-    { $Dict = New-Object "System.Collections.Generic.Dictionary[System.String, System.Object]"($Dict, [StringComparer]::OrdinalIgnoreCase) }
-  }
-
-  return $Dict
-}
-#>
-
 <#
   .SYNOPSIS
     Helper function to take a JSON string and turn it into a hashtable
@@ -573,7 +518,6 @@ function ConvertFrom-JsonToHashtable
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 #>
-#<#
 function ConvertFrom-JsonToHashtable
 {
   param (
@@ -634,7 +578,6 @@ function ConvertFrom-JsonToHashtable
   # Recurse
   ConvertToHashtable ($InputObject | ConvertFrom-Json)
 }
-##>
 #endregion
 
 #region ConvertFrom-HashtableToPSObject
